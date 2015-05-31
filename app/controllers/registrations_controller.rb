9db0353755a217ed556e9d4ece7e6cf53e22f8bf
Tomasz Bernaciak <t.bernaciak@binarapps.com>
2015-05-31 21:42:12 +0200

    change in registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::SessionsController
   respond_to :json
 
   # Path: /registrations
-  # POST - User & Admin - Registration action. User can register to application or Admin can add the user.
-  # @param [JSON] params data: { user: { email, password, password_confirmation, name, surname, position_id, admin } }
+  # POST - Registration action. User can register to application or Admin can add the user. admin is a boolean field, send true if user is an admin.
+  # @param [JSON] params data: { email, password, password_confirmation, name, surname, position_id, admin }
   def create
-    user = User.new(user_params)
+    user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], name: params[:name], surname: params[:surname], position_id: params[:position_id], admin: params[:admin])
     if user.save
       render status: 200, json: user
     else
@@ -16,11 +16,4 @@ class RegistrationsController < Devise::SessionsController
       }
     end
   end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :name, :surname, :position_id, :admin)
-  end
-
 end
