8ba7c62fbbcf3749f0adf64e2fa90dde255dadf4
Tomasz Bernaciak <t.bernaciak@binarapps.com>
2015-06-11 22:07:32 +0200

    self

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   has_many :productions
   belongs_to :manager, :class_name => "User"
 
-  def check(params)
+  def self.check(params)
     puts params
   end
 end
