8db838bb7c161152687c53f5bea4c658b251afad
Tomasz Bernaciak <t.bernaciak@binarapps.com>
2015-06-15 11:27:39 +0200

    auth token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613202953) do
+ActiveRecord::Schema.define(version: 20150615092632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,8 +79,10 @@ ActiveRecord::Schema.define(version: 20150613202953) do
     t.integer  "position_id"
     t.boolean  "admin"
     t.integer  "project_id"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
